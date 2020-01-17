35c45845824d845cd7fe4adc06c1a3e30774f075
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2020-01-17 00:17:10 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -4,11 +4,12 @@ class Dog
   attr_accessor :name, :breed, :age 
   
   @@all = []
+  
  def initialize(name,breed,age)
    @name = name
    @breed = breed
    @age = age
-   @@all < self
+   @@all << self
  end
  
  def self.all 
