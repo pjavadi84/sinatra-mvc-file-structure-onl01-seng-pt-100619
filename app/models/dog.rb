52a951c94c8a1ace6d98682ae21b5e8a3ccf9d17
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2020-01-17 00:15:06 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,9 +3,7 @@ require 'pry'
 class Dog 
   attr_accessor :name, :breed, :age 
   
- def initialize(params = {:name :breed :age})
-    params.each { |key, value| send "#{key}=", value }
-    binding.pry
+ def initialize
  end
 
 end 
